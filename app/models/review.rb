65db7904fcaaa30cf59b4896dad112a722a67201
Student <example@example.com>
2020-01-25 13:50:52 -0800

    add validations to product and review models and spec

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,10 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :author, presence: true
+  validates_length_of :content_body, minimum: 50
+  validates_length_of :content_body, maximum: 250
+  # validates :rating, greater_than_or_equal_to: 1
+  # validates :rating, less_than_or_equal_to: 5
 
-  validates :product_id, presence: true
+
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe Review do
   it { should belong_to(:product) }
-end
+  it { should validate_length_of(:content_body).is_at_least(50) }
+  it { should validate_length_of(:content_body).is_at_most(250) }
+  it { should validate (:rating).greater_than_or_equal_to(1)}
+  it { should validate (:rating).greater_than_or_equal_to(5)}
+  end
