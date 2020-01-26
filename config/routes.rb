a390a8a59c821f4e2b3504e7238053a0dccd2ab2
bryon burke <bryonburke@bryons-Mac-mini.local>
2020-01-26 14:55:02 -0800

    add bcrypt, troubleshooting undefined method password salt error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'products#index'
+  # root to: 'products#index'
+  root to: 'users#new'
+
   resources :products do
     resources :reviews, except: [:destroy]
   end
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 end
