acbe935deda6ae2cefb93dc88c7b7e721df8d6b5
bryon burke <bryonburke@bryons-MBP.hsd1.or.comcast.net>
2020-02-01 15:24:39 -0800

    Add reviews to products in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
+# Product.destroy_all
 #
-# Examples:
+# 50.times do |i|
+#   Product.create!(
+#     name: Faker::Food.dish,
+#     cost: Faker::Number.decimal(l_digits: 2),
+#     country_of_origin: Faker::Nation.nationality)
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Product.destroy_all
-
-50.times do |index|
-  Product.create!(name: Faker::Food.dish,
-                        cost: Faker::Number.decimal(l_digits: 2),
-                        country_of_origin: Faker::Nation.nationality)
-                        # image_url: "site.com/here_is_a_picture_of_cardamom.jpg")
-end
-
-# Review.destroy_all
-#
-# 250.times do |index|
-#   Review.create!(author: Faker::Name.name,
-#                         content_body: Faker::Lorem.sentence,
-#                         rating: Faker::Number.between(from: 1, to: 5))
-#                         # image_url: "site.com/here_is_a_picture_of_cardamom.jpg")
+#     5.times do |j|
+#         Review.create!(
+#           author: Faker::Name.name,
+#           content_body: Faker::Lorem.characters(number: 60),
+#           rating: Faker::Number.between(from: 1, to: 5))
+#     end
 # end
+
+class Seed
+
+  Product.destroy_all
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_seed
+  end
+
+  def generate_seed
+    50.times do |i|
+      products = []
+      product = Product.create!(
+        name: Faker::Food.dish,
+        cost: Faker::Number.decimal(l_digits: 2),
+        country_of_origin: Faker::Nation.nationality)
+        products.push(product)
+
+        5.times do |j|
+          products.each do |d|
+            review = Review.create!(
+              author: Faker::Name.name,
+              content_body: Faker::Lorem.characters(number: 60),
+              rating: Faker::Number.between(from: 1, to: 5),
+              :product_id => d.id)
+
+            end
+          end
+        end
+      end
+
+    end
+
+    Seed.begin
